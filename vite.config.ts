76d7acd2b6acc2414c1e9c578eec1a4f8d351dca
fancn21th <fancn21th@gmail.com>
2024-10-12 16:06:03 +0800

    📦 NEW: first match

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { parse } from "@babel/parser";
 import * as babelTraverse from "@babel/traverse";
+import * as babelGenerate from "@babel/generator";
 
-// import generate from "@babel/generator";
+const traverse = babelTraverse.default;
+const generate = babelGenerate.default;
 
 function tagText() {
   return {
@@ -13,21 +15,31 @@ function tagText() {
       // 只处理 .tsx 文件
       if (id.endsWith(".tsx")) {
         console.log("处理文件", id);
+
         const ast = parse(code, {
           sourceType: "module",
           plugins: ["typescript", "jsx"],
         });
 
-        babelTraverse.default.default(ast, {
+        traverse.default(ast, {
           JSXElement(path) {
             path.node.children.forEach((child) => {
               if (child.type === "JSXText" && child.value.trim().length > 0) {
                 // 为文本增加计数器
                 console.log("文本内容", child.value);
+                child.value = `${child.value}@marked`;
               }
             });
           },
         });
+
+        // 使用 @babel/generator 生成修改后的代码
+        const output = generate.default(ast, {}, code).code;
+
+        // 写入修改后的代码到文件
+        // fs.writeFileSync(id, output, "utf-8");
+
+        return output; // 返回修改后的代码
       }
 
       return code;
