80ed543df536695e1f42fa1357cd269f28107eb8
fancn21th <fancn21th@gmail.com>
2024-10-13 19:57:19 +0800

    📦 NEW: 组件的嵌套

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -1,12 +1,9 @@
-import Sample from "./components/Sample";
-import "./App.css";
-
 enum Colors {
   key1 = "中文文本",
   "中文Key" = "中文文本",
 }
 
-function App() {
+function Sample() {
   const text = "中文文本";
   const text2 = `${text} | 中文文本`;
 
@@ -19,10 +16,8 @@ function App() {
       <input type="text" value="中文文本" />
       <div>{Colors.key1}</div>
       <div>{Colors.中文Key}</div>
-      <hr />
-      <Sample />
     </>
   );
 }
 
-export default App;
+export default Sample;
